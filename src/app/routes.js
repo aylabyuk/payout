2dd733ee440de764a48b83078139e7a9b2ad0ca1
thevinci <oriel.absin@gmail.com>
2018-01-26 19:05:35 +0800

    enable redux-persist

--- src/app/routes.js
+++ src/app/routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
-import { persistCombineReducers } from 'redux-persist'
+import { persistCombineReducers, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import { Route } from 'react-router-dom'
 import { routerReducer, ConnectedRouter, routerMiddleware } from 'react-router-redux'
@@ -60,6 +60,8 @@ const store = createStore(
     enhancer
   )
 )
+
+let persistor = persistStore(store)
 
 export const Routes = () => {
   return(
